c57739316ef9ccf89cb6cc799c2ef9a07036c42d
dynae <nightcorebanksy@gmail.com>
2025-01-22 15:05:30 -0400

    tilemap more fleshed out, pixel perfect camera added, tile seam lines fixed with atlas

--- Assets/Packages/CharacterController/PlayerMovement.cs
+++ Assets/Packages/CharacterController/PlayerMovement.cs
@@ -1,52 +1,29 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	// private PlayerInputActions inputActions;
 	private Vector2 _playerMovement;
 	private Rigidbody2D _rigidbody2D;
 	[SerializeField]
 	private float moveSpeed = 10f;
 	
-	
-
-	private void Interact()
-	{
-		throw new NotImplementedException();
-	}
-
-	private void HandleMovement(Vector2 moveInput)
-	{
-		// Get movement input from the Input Actions
-		_playerMovement = moveInput;
-		// Calculate movement direction based on input
-		// Vector2 moveDirection = new Vector2(_moveInput.x, _moveInput.y).normalized;
-	}
-	
+	// movement / interact methods
+	private void Interact() { throw new NotImplementedException(); }
+	private void HandleMovement(Vector2 moveInput) { _playerMovement = moveInput; }
 	private void FixedUpdate()
 	{
-		// Move the object
 		transform.Translate(_playerMovement * (moveSpeed * Time.deltaTime), Space.World);
 	}
-
-	private void Start()
-	{
-		_rigidbody2D = GetComponent<Rigidbody2D>();
-	}
 	
-	private void OnEnable()
-	{
+	// initiating / reference handling
+	private void Start() { _rigidbody2D = GetComponent<Rigidbody2D>(); }
+	private void OnEnable() {
 		ActionsManager.MoveEvent += HandleMovement; 
 		ActionsManager.InteractEvent += Interact;
-		
 	}
-	private void OnDisable()
-	{
+	private void OnDisable() {
 		ActionsManager.MoveEvent -= HandleMovement;
 		ActionsManager.InteractEvent -= Interact;
 	}
-
 }
